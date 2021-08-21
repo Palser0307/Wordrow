a93f61b8fad0e3290fbcd213249b9814f16216e4
Palser0307 <youma.thetto0307@gmail.com>
2021-08-21 21:10:11 +0900

    add
    床，移動，ジャンプ(仮)を実装
    重力に関する計算が難しい

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour{
+    private CharacterController characterController;
+    private Vector3 moveDirection;
+    public float JumpPower; // default: 0.5
+    public float FallRate;  // default: 4
+
+    void Start(){
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(characterController.isGrounded){
+            if(OVRInput.GetDown(OVRInput.RawButton.A)){
+                moveDirection.y = JumpPower;
+                Debug.Log("Jump");
+            }
+        }
+        Debug.Log(moveDirection.y);
+        if(moveDirection.y < 0){
+            moveDirection.y = 0;
+        }else{
+            moveDirection.y += (Physics.gravity.y * Time.deltaTime) / FallRate;
+        }
+        characterController.Move(moveDirection);
+    }
+}
