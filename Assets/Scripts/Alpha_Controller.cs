0c5a3dca52366e0bc99970b6bfb3ae03ed2633c9
Palser0307 <youma.thetto0307@gmail.com>
2021-09-04 16:18:17 +0900

    add HUD for Alpha
    あーちゃん用HUDベースの追加
    機能追加はこれから

---
+++ Assets/Scripts/Alpha_Controller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alpha_Controller : MonoBehaviour{
+    [SerializeField]
+    GameObject HUD_Alpha;
+
+    void Start(){
+
+    }
+
+    void Update(){
+
+    }
+}
