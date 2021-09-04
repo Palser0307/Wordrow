9a617bb83d69fd34155e1ef6b7735420098fd745
Palser0307 <youma.thetto0307@gmail.com>
2021-09-04 17:12:13 +0900

    add Alpha_Controller.cs
    Alpha_Controller.cs：あーちゃんを制御するスクリプト を追加
    セリフ一覧をロードする機能を実装予定だが，セリフ一覧の形式が未定

--- Assets/Scripts/Alpha_Controller.cs
+++ Assets/Scripts/Alpha_Controller.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class Alpha_Controller : MonoBehaviour{
     [SerializeField]
-    GameObject HUD_Alpha;
+    protected GameObject Alpha_HUD;
+
+    protected GameObject alpha_avater;
+    protected GameObject alpha_text;
+
+    protected string[] alpha_wordsArray;
 
     void Start(){
 
@@ -12,5 +17,33 @@ public class Alpha_Controller : MonoBehaviour{
 
     void Update(){
 
+    }
+
+    // 文字送り(つまり次のセリフへの遷移)
+    // nextWord? 関数名わからん
+    // 多分Controller.csから呼ばれると思う
+    public void next(){
+
+    }
+
+    // セリフをロード
+    // Resources/Alpha/ file_name のデータをロード
+    void loadWords(string file_name){
+
+    }
+
+    // HUD_Alpha turn off
+    public void turnOff(){
+        Alpha_HUD.SetActive(false);
+    }
+
+    // HUD_Alpha turn on
+    public void turnOn(){
+        Alpha_HUD.SetActive(true);
+    }
+
+    // HUD_Alpha is active?
+    public bool getActive(){
+        return Alpha_HUD.activeSelf;
     }
 }
