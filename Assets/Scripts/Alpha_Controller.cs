df215198b40af7e5d4fa1e35465ec3f199a8258f
Palser0307 <youma.thetto0307@gmail.com>
2021-09-10 21:30:26 +0900

    add alpha_ctrl
    あーちゃんコントローラにちょいちょい追加
    ここからJSON読み込みとか追加する

--- Assets/Scripts/Alpha_Controller.cs
+++ Assets/Scripts/Alpha_Controller.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// in "System_Scripts" Object
 public class Alpha_Controller : MonoBehaviour{
     [SerializeField]
     protected GameObject Alpha_HUD;
@@ -11,19 +12,26 @@ public class Alpha_Controller : MonoBehaviour{
 
     protected string[] alpha_wordsArray;
 
-    void Start(){
+    // in System_Scripts Object
+    protected GameObject Sys_Scripts = null;
 
+    void Start(){
+        Sys_Scripts = GameObject.Find("System_Scripts");
     }
 
     void Update(){
-
+        if(OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger)){
+            Debug.Log("run : next()");
+            Debug.Log(Sys_Scripts.GetComponent<Controller>().GetLHold());
+            next();
+        }
     }
 
     // 文字送り(つまり次のセリフへの遷移)
     // nextWord? 関数名わからん
     // 多分Controller.csから呼ばれると思う
     public void next(){
-
+        Debug.Log("run : next()");
     }
 
     // セリフをロード
