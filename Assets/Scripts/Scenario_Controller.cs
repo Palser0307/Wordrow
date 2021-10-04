86122fe8f6d10372b3946f49411802c3b7595cd5
Palser0307 <youma.thetto0307@gmail.com>
2021-10-05 00:14:57 +0900

    add Scenario_Ctrler
    Scenario_Controllerを追加
    中身はまだあんまり
    一応statusから各Scriptに反映
    
    あーちゃんCtrlにactive/inactiveのstatusを埋め込み

---
+++ Assets/Scripts/Scenario_Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scenario_Controller : MonoBehaviour{
+    // シナリオ status
+    // alpha : あーちゃんセリフ待ち
+    //
+    public static string status;
+    // Start is called before the first frame update
+    void Start(){
+        status = "alpha";
+    }
+
+    // Update is called once per frame
+    void Update(){
+        // this.statusを各Scriptに反映
+        switch (status){
+            case "alpha" :
+                Alpha_Controller.setStatus("active");
+                break;
+            default:
+                break;
+        }
+    }
+}
