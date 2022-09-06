d663a2ab1d6f1f6ab126dff868d63400dc5f4ae7
Taichi_Murakami <52375483+PuruPuruCK@users.noreply.github.com>
2022-09-07 03:49:06 +0900

    place_objects_round_wall system compleate
    はめ込みシステムの基礎は完成した
    gen2にTrigger用のコリジョンを追加した

--- Assets/PlaceObjectsRoundWall/Cards/card_createShapes_gen2.cs
+++ Assets/PlaceObjectsRoundWall/Cards/card_createShapes_gen2.cs
@@ -32,6 +32,8 @@ public class card_createShapes_gen2 : TD_card_class
     // 実際の効果
     // 台座（Target）の上にShapeを生成する
     // 発動対象タグを持っているかはOnCollisionEnter()で確認済み
+
+    //collisionのほうは使わないので必要ないです
     public override void use(Collision other){
         outputLog("use func. start.");
 
@@ -50,10 +52,33 @@ public class card_createShapes_gen2 : TD_card_class
         playAudio();
 
         //collision のTransformの位置にShapeを召喚
+        Transform trans= other.gameObject.transform.Find("PlayerAnswer_Pillar").GetComponent<Transform>();
 
-        Transform trans= other.gameObject.GetComponent<Transform>();
+        Vector3 vecrot=trans.rotation.eulerAngles;
 
-        Instantiate(createSprite,trans.position,trans.rotation);
+        vecrot.y=90;
+
+        Vector3 vecpos = trans.position;
+
+        vecpos.y = 1.5f;
+
+        //生成
+        var gosp = Instantiate(createSprite,vecpos,Quaternion.Euler(vecrot),trans) as GameObject;
+        //名前をセット
+        gosp.name = createSprite.name;
+
+        GameObject go = other.gameObject;
+
+        //生成したよフラグ
+        if(other.gameObject.TryGetComponent(out QuizSystem  QuizSystem)){
+            other.gameObject.GetComponent<QuizSystem>().isPlayerAnswerGenerated = true;
+        }
+        
+
+
+        //カードを離して消滅させる
+        this.gameObject.GetComponent<OVRGrabbable>().grabbedBy.ForceRelease(this.gameObject.GetComponent<OVRGrabbable>());
+        Destroy(this.gameObject);
 
     }
 }
